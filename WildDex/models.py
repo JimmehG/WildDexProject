5f19331e551c020cd28043ca5b482ac4adf31076
JimmehG <j.gronowski@gmail.com>
2016-04-13 01:03:07 +1000

    reformatted lines

--- WildDex/models.py
+++ WildDex/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 # Create your models here.
 # ID CREATED AUTOMATICALLY.
 
@@ -8,8 +9,8 @@ class Animal(models.Model):
     species = models.CharField(max_length=64)
     # prev_owners = needs array of previous owners
 
+
 class Caller(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     phone_number = PhoneNumberField()
-
