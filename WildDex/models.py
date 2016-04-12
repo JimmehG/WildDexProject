cfe864e9a877e3e8a8fa5b53fe18827e7e76230b
JimmehG <j.gronowski@gmail.com>
2016-04-13 01:02:23 +1000

    added Animal and Caller models, still need more fields for both

--- WildDex/models.py
+++ WildDex/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+# ID CREATED AUTOMATICALLY.
+
+class Animal(models.Model):
+    species = models.CharField(max_length=64)
+    # prev_owners = needs array of previous owners
+
+class Caller(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    phone_number = PhoneNumberField()
+
