ec5ce46e5e67976b0b9a22e233c5c4822d18ec7f
JimmehG <j.gronowski@gmail.com>
2016-04-13 18:25:08 +1000

    Removed caller (added caller info in Animal model) added more Animal fields, OfficeVolunteer, Carer, Branch manager

--- WildDex/models.py
+++ WildDex/models.py
@@ -6,11 +6,34 @@ from phonenumber_field.modelfields import PhoneNumberField
 # ID CREATED AUTOMATICALLY.
 
 class Animal(models.Model):
-    species = models.CharField(max_length=64)
+    SPECIES_CHOICES = (
+        ('RTP', 'Ring Tail Possum'),
+        # add species in here
+    )
+    species = models.CharField(max_length=64,choices=SPECIES_CHOICES)
+    caller_first_name = models.CharField(max_length=64)
+    caller_last_name = models.CharField(max_length=64)
+    caller_phone_number = PhoneNumberField()
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     # prev_owners = needs array of previous owners
 
 
-class Caller(models.Model):
+class OfficeVolunteer(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+
+
+class Carer(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email_address = models.EmailField()
     phone_number = PhoneNumberField()
+
+
+class BranchManager(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
