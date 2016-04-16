9a5d824f0defd141b9be6b846bcfe8dfe0a6205b
KaiyeZhang <zhangky2011@gmail.com>
2016-04-16 23:43:06 +1000

    just add a about page for the project. try to add an index page as home page. but failed this pused version is without bugs

--- WildDex/views.py
+++ WildDex/views.py
@@ -13,7 +13,7 @@ from .forms import PersonForm
 def index(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
+     # create a form instance and populate it with data from the request:
         form = PersonForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
@@ -22,8 +22,13 @@ def index(request):
             # redirect to a new URL:
             return HttpResponseRedirect('/wilddex/')
 
+
     # if a GET (or any other method) we'll create a blank form
     else:
         form = PersonForm()
 
     return render(request, 'personform.html', {'form': form})
+
+def about(request):
+    #return HttpResponse("about")
+    return render(request, 'about.html', {})
