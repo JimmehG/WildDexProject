c87d4607c1c7531dd9b6c84707dd88d7c49fc3ab
KaiyeZhang <zhangky2011@gmail.com>
2016-04-30 15:44:34 +1000

    Added  a register function by the "User" model in Django Added  a register html for the function

--- WildDex/urls.py
+++ WildDex/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),  # www.sydneywilddex.com.au/
+    url(r'^$|^index/$', views.index, name='index'),  # www.sydneywilddex.com.au/
     url(r'^about/$', views.about, name='about'),  # www.sydneywilddex.com.au/about
     url(r'^add_user/$', views.add_user, name='add_user'),  # www.sydneywilddex.com.au/add_user
     url(r'^submitted/$', views.submitted, name='submitted'),  # etc.
     url(r'^table/$', views.table, name='table'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
 ]
